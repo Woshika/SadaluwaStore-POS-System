b74dc3f6619949ac4ec6b19aa4ebe4ec416f2f7b
Woshika <wkavindi016@gmail.com>
2023-12-28 08:04:21 -1200

    customer package created

--- src/com/devstack/pos/dao/custom/CustomerDao.java
+++ src/com/devstack/pos/dao/custom/CustomerDao.java
@@ -1,4 +1,18 @@
 package com.devstack.pos.dao.custom;
 
+import com.devstack.pos.entity.Customer;
+
+import java.util.List;
+
 public interface CustomerDao {
+
+    public boolean saveCustomer(Customer customer);
+
+    public boolean updateCustomer(Customer customer);
+
+    public boolean deleteCustomer(String email);
+
+    public Customer findCustomer(String email);
+
+    public List<Customer> findAllCustomers();
 }
