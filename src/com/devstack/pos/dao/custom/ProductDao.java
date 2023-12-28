ffe9164c1d49a2908e8170008f287e5b76f6c71d
Woshika <wkavindi016@gmail.com>
2023-12-28 08:16:42 -1200

    customer package created

--- src/com/devstack/pos/dao/custom/ProductDao.java
+++ src/com/devstack/pos/dao/custom/ProductDao.java
@@ -1,4 +1,18 @@
 package com.devstack.pos.dao.custom;
 
+import com.devstack.pos.entity.Product;
+
+import java.util.List;
+
 public interface ProductDao {
+
+    public boolean saveProduct(Product customer);
+
+    public boolean updateProduct(Product customer);
+
+    public boolean deleteProduct(int code);
+
+    public Product findProduct(int code);
+
+    public List<Product> findAllProducts();
 }
