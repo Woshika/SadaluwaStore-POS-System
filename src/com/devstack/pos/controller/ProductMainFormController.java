883a349ac90ebf3736f29a00543038ce4aaa0284
Woshika <wkavindi016@gmail.com>
2023-12-27 15:29:43 -1200

    Created Product UI

---
+++ src/com/devstack/pos/controller/ProductMainFormController.java
@@ -0,0 +1,19 @@
+package com.devstack.pos.controller;
+
+import com.jfoenix.controls.JFXTextField;
+import javafx.event.ActionEvent;
+import javafx.scene.control.TextArea;
+
+public class ProductMainFormController {
+    public JFXTextField txtProductCode;
+    public TextArea txtProductDescription;
+
+    public void btnBackToHomeOnAction(ActionEvent actionEvent) {
+    }
+
+    public void btnNewCustomerOnAction(ActionEvent actionEvent) {
+    }
+
+    public void btnNewProductOnAction(ActionEvent actionEvent) {
+    }
+}
